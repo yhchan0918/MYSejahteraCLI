f5b52daf67363cbb21e4130373d687b0a8e78c35
unknown <ahboe579@gmail.com>
2020-12-26 01:36:50 +0800

    customer view ok

--- AdminMain.java
+++ AdminMain.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.ArrayList;
 
 public class AdminMain {
@@ -45,10 +46,10 @@ public class AdminMain {
         int choice = Utils.getUserChoice(options);
         switch (choice) {
             case 1:
-                // customer view by admin
+                customerView();
                 break;
             case 2:
-                // shop view by admin
+                shopView();
                 break;
             case 3:
                 // master view
@@ -61,9 +62,40 @@ public class AdminMain {
 
     private static void flag() {
         System.out.println("flag someone");
+
+        // 1. after flagging a customer as case, need to all checking
+        // 2. after adding a new visit manually (UserMain)
     }
 
     private static void generate() {
         System.out.println("generate 30 visits");
     }
+
+    private static void customerView() throws Exception {
+        Utils.displayHeader("List of Customers Details");
+
+        // Convert ArrayList<> to hashMap AryList for displaying data
+        ArrayList<HashMap<String, String>> hashMapCustomerList = new ArrayList<HashMap<String, String>>();
+        ArrayList<Customer> customerList = Utils.readListFromFile("customers");
+        String[] col = { "No", "Name", "Phone", "Status" };
+
+        for (int i = 0; i < customerList.size(); i++) {
+            int index = i + 1;
+            HashMap<String, String> map = new HashMap<>();
+            map.put("No", Integer.toString(index));
+            map.put("Name", customerList.get(i).getName());
+            map.put("Phone", customerList.get(i).getPhone());
+            map.put("Status", customerList.get(i).getStatus());
+            hashMapCustomerList.add(map);
+        }
+        Table.display(col, hashMapCustomerList);
+
+        // goBack 2.0 with csv output
+    }
+
+    private static void shopView() throws Exception {
+        // same thinggy
+    }
+
+    // closeContactTracing in Utils.java
 }
