4fa838f90e334bff432da8e0ed07264108ece0d4
Chan Yun Hong <yhchan0918@gmail.com>
2020-12-28 14:56:45 +0800

    change preload customer sequence

--- Initializer.java
+++ Initializer.java
@@ -9,10 +9,10 @@ public class Initializer {
 
   public static void initCust() throws Exception {
     ArrayList<Customer> customersList = new ArrayList<Customer>();
-    customersList.add(new Customer("YHChan", "012456789"));
     customersList.add(new Customer("Boe", "0155547899", "Case"));
-    customersList.add(new Customer("Neoh", "0158553685", "Close"));
+    customersList.add(new Customer("YHChan", "012456789"));
     customersList.add(new Customer("Delphine", "0136744547", "Normal"));
+    customersList.add(new Customer("Neoh", "0158553685", "Close"));
     customersList.add(new Customer("Jason", "0177876345", "Normal"));
 
     Utils.saveToFile(customersList, Record.customerFilename);
