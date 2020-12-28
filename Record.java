262d39cce3720e9c4139f683f164a1ab6c6a8195
Chan Yun Hong <yhchan0918@gmail.com>
2020-12-28 16:29:58 +0800

    change accesor

--- Record.java
+++ Record.java
@@ -1,6 +1,6 @@
 import java.io.Serializable;
 
-public class Record implements Serializable {
+abstract class Record implements Serializable {
 
   private static final long serialVersionUID = 1L;
   public final static String visitFilename = "Visits";
