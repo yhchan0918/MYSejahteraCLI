282001d044e27e7cd2be975b568f18dc253ccf05
Chan Yun Hong <yhchan0918@gmail.com>
2020-12-29 10:53:17 +0800

    add file

---
+++ AdminView.java
@@ -0,0 +1,100 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class AdminView {
+  private final static String masterVisitFilename = "MasterVisit";
+  private final static String custViewFilename = "AdminCustView";
+  private final static String shopViewFilename = "AdminShopView";
+
+  public static void displayCustomer() throws Exception {
+    Utils.displayHeader("List of Customers Details");
+
+    // Convert ArrayList<> to hashMap AryList for displaying data
+    ArrayList<HashMap<String, String>> hashMapCustomerList = new ArrayList<HashMap<String, String>>();
+    ArrayList<Customer> customerList = Utils.readListFromFile(Record.customerFilename);
+    String[] col = { "No", "Name", "Phone", "Status" };
+
+    for (int i = 0; i < customerList.size(); i++) {
+      int index = i + 1;
+      HashMap<String, String> map = new HashMap<>();
+      map.put("No", Integer.toString(index));
+      map.put("Name", customerList.get(i).getName());
+      map.put("Phone", customerList.get(i).getPhone());
+      map.put("Status", customerList.get(i).getStatus());
+      hashMapCustomerList.add(map);
+    }
+    Table.display(col, hashMapCustomerList);
+
+    Utils.displayExportCSVPrompt(col, hashMapCustomerList, custViewFilename);
+    backMenu();
+  }
+
+  public static void displayShop() throws Exception {
+    Utils.displayHeader("List of Shops Details");
+
+    ArrayList<HashMap<String, String>> hashMapShopList = new ArrayList<HashMap<String, String>>();
+    ArrayList<Shop> shopList = Utils.readListFromFile(Record.shopFilename);
+    String[] col = { "No", "Name", "Phone", "Manager", "Status" };
+
+    for (int i = 0; i < shopList.size(); i++) {
+      int index = i + 1;
+      HashMap<String, String> map = new HashMap<>();
+      map.put("No", Integer.toString(index));
+      map.put("Name", shopList.get(i).getName());
+      map.put("Phone", shopList.get(i).getPhone());
+      map.put("Manager", shopList.get(i).getManager());
+      map.put("Status", shopList.get(i).getStatus());
+      hashMapShopList.add(map);
+    }
+    Table.display(col, hashMapShopList);
+
+    Utils.displayExportCSVPrompt(col, hashMapShopList, shopViewFilename);
+    backMenu();
+
+  }
+
+  public static void displayMasterVisit() throws Exception {
+    Utils.displayHeader("Master Visit History");
+
+    ArrayList<HashMap<String, String>> hashMapMasterVisit = new ArrayList<HashMap<String, String>>();
+    ArrayList<Visit> masterList = Utils.readListFromFile(Record.visitFilename);
+    String[] col = { "No", "Date", "Time", "Customer", "Shop" };
+
+    for (int i = 0; i < masterList.size(); i++) {
+      int index = i + 1;
+      HashMap<String, String> map = new HashMap<>();
+      map.put("No", Integer.toString(index));
+      map.put("Date", masterList.get(i).getDate());
+      map.put("Time", masterList.get(i).getTime());
+      map.put("Customer", masterList.get(i).getCustomer());
+      map.put("Shop", masterList.get(i).getShop());
+      hashMapMasterVisit.add(map);
+    }
+    Table.display(col, hashMapMasterVisit);
+
+    Utils.displayExportCSVPrompt(col, hashMapMasterVisit, masterVisitFilename);
+    backMenu();
+
+  }
+
+  private static void backMenu() throws Exception {
+
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("Please choose an option");
+    System.out.println("1. Go Back to View Data Menu");
+    System.out.println("2. Go to Main Menu");
+
+    List<Integer> options = Arrays.asList(1, 2);
+    int choice = Utils.getUserChoice(options);
+    switch (choice) {
+      case 1:
+        AdminMain.viewMenu();
+        break;
+      case 2:
+        AdminMain.adminMainMenu();
+        break;
+    }
+  }
+}
---
+++ UserMenu.java
@@ -0,0 +1,208 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserMenu {
+  private static Customer currentCustomer;
+  private static Shop currentShop;
+  private static ArrayList<Shop> shopslist;
+
+  public static void main() throws Exception {
+    System.out.println("Loading......");
+    shopslist = Utils.readListFromFile(Record.SHOP_FILENAME);
+    displayMainMenu();
+  }
+
+  public static void displayMainMenu() throws Exception {
+    Utils.displayHeader("Welcome to Contact Tracing System For COVID-19");
+    System.out.println("| Main Menu |");
+    System.out.println("Please Type the No of your desired action and press ENTER to proceed");
+    System.out.println("1. Sign In as Customer role");
+    System.out.println("2. Sign In as Shop role");
+    System.out.println("3. Register as New Customer");
+    System.out.println("4. Exit Program");
+    List<Integer> options = Arrays.asList(1, 2, 3, 4);
+    int choice = Utils.getUserChoice(options);
+    switch (choice) {
+      case 1:
+        signIn("Customer");
+        break;
+      case 2:
+        signIn("Shop");
+        break;
+      case 3:
+        register();
+        break;
+      case 4:
+        System.out.println("Exit...");
+        System.exit(0);
+        break;
+    }
+  }
+
+  private static void register() throws Exception {
+    ArrayList<Customer> customerslist = Utils.readListFromFile(Record.CUSTOMER_FILENAME);
+    Utils.displayHeader("Register As New Customer");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter new customer name");
+    String name = input.nextLine();
+    System.out.println("Enter new customer phone number");
+    String phone = input.nextLine();
+    Customer newCustomer = new Customer(name, phone);
+    customerslist.add(newCustomer);
+    Utils.saveToFile(customerslist, Record.CUSTOMER_FILENAME);
+    currentCustomer = newCustomer;
+    System.out.println("Successfully Register!!" + "\n");
+    displayCustomerMenu();
+  }
+
+  private static void signIn(String role) throws Exception {
+    String title = "Sign In as " + role + " role";
+    Utils.displayHeader(title);
+    Scanner input = new Scanner(System.in);
+    String prompt = role.equals("Customer") ? "Enter Your Name" : "Enter Your Shop Name";
+
+    while (true) {
+      System.out.println(prompt + " (Note: Case Sensitive)");
+      String name = input.nextLine();
+
+      if (validateUser(role, name)) {
+        System.out.println("Successfully Login!!" + "\n");
+        if (role.equals("Customer")) {
+          displayCustomerMenu();
+        } else {
+          displayShopMenu();
+        }
+        break;
+      } else {
+        System.out.println("Invalid Credentials. Please Try Again");
+        continue;
+      }
+    }
+  }
+
+  private static boolean validateUser(String role, String name) throws Exception {
+    ArrayList<Customer> customerslist = Utils.readListFromFile(Record.CUSTOMER_FILENAME);
+    if (role.equals("Customer")) {
+      for (Customer customer : customerslist) {
+        if (customer.getName().equals(name)) {
+          currentCustomer = customer;
+          return true;
+        }
+      }
+      return false;
+    } else {
+      for (Shop shop : shopslist) {
+        if (shop.getName().equals(name)) {
+          currentShop = shop;
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+
+  private static void displayCustomerMenu() throws Exception {
+    Utils.displayHeader("Customer Menu");
+    System.out.println("Please Type the No of your desired action and press ENTER to proceed");
+    System.out.println("1. Check In Shop");
+    System.out.println("2. View the history of the shops you have visted");
+    System.out.println("3. View Your Status");
+    System.out.println("4. Logout");
+    List<Integer> options = Arrays.asList(1, 2, 3, 4);
+    int choice = Utils.getUserChoice(options);
+    switch (choice) {
+      case 1:
+        checkIn();
+        break;
+      case 2:
+        viewVisitsOfCustomer();
+        break;
+      case 3:
+        viewCustomerStatus();
+        break;
+      case 4:
+        displayMainMenu();
+        break;
+    }
+  }
+
+  private static void checkIn() throws Exception {
+    ArrayList<Visit> visitslist = Utils.readListFromFile(Record.VISIT_FILENAME);
+    Utils.displayHeader("Check In Shop");
+    System.out.println("Please Type the No of the Shop you want to check in");
+    List<Integer> options = new ArrayList<Integer>();
+    for (int i = 0; i < shopslist.size(); i++) {
+      int index = i + 1;
+      options.add(index);
+      System.out.println(index + ". " + shopslist.get(i).getName());
+    }
+    int choice = Utils.getUserChoice(options);
+    visitslist.add(new Visit(currentCustomer.getName(), shopslist.get(choice - 1).getName()));
+    Utils.saveToFile(visitslist, Record.VISIT_FILENAME);
+    System.out.println("Successfully Check In!!");
+    displayGoBackMenu();
+  }
+
+  private static void viewVisitsOfCustomer() throws Exception {
+    Utils.displayHeader("View History of the shops you have visted");
+    ArrayList<Visit> visitslist = Utils.readListFromFile(Record.VISIT_FILENAME);
+    // Filter visitslist by currentCustomer name
+    visitslist.removeIf(visit -> !(visit.getCustomer().equals(currentCustomer.getName())));
+
+    // Check whether user have checked any shop
+    if (visitslist.isEmpty()) {
+      System.out.println("You have not checked in any shop yet");
+    } else {
+      // Convert ArrayList<Visit> to hashMap arraylist
+      ArrayList<HashMap<String, String>> hashMapVisitsList = new ArrayList<HashMap<String, String>>();
+      String[] colNamesList = { "No", "Date", "Time", "Shop" };
+      for (int i = 0; i < visitslist.size(); i++) {
+        int index = i + 1;
+        HashMap<String, String> map = new HashMap<>();
+        map.put("No", Integer.toString(index));
+        map.put("Date", visitslist.get(i).getDate());
+        map.put("Time", visitslist.get(i).getTime());
+        map.put("Shop", visitslist.get(i).getShop());
+        hashMapVisitsList.add(map);
+      }
+      // Print Table
+      Table.display(colNamesList, hashMapVisitsList);
+      // Display Export CSV Prompt
+      Utils.displayExportCSVPrompt(colNamesList, hashMapVisitsList, currentCustomer.getName() + Record.VISIT_FILENAME);
+    }
+
+    displayGoBackMenu();
+  }
+
+  private static void viewCustomerStatus() throws Exception {
+    Utils.displayHeader("View Customer Status");
+    System.out.println("Your Status: " + currentCustomer.getStatus());
+    displayGoBackMenu();
+  }
+
+  private static void displayGoBackMenu() throws Exception {
+
+    System.out.println("-------------------------------------------");
+    System.out.println("Press Enter key to go back Customer Menu...");
+    try {
+      System.in.read();
+      displayCustomerMenu();
+    } catch (Exception e) {
+    }
+  }
+
+  private static void displayShopMenu() {
+    Utils.displayHeader("View Shop Status");
+    System.out.println("Your Shop Status: " + currentShop.getStatus());
+    System.out.println("---------------------------------------");
+    System.out.println("Press Enter key to go back Main Menu...");
+    try {
+      System.in.read();
+      displayMainMenu();
+    } catch (Exception e) {
+    }
+  }
+}
