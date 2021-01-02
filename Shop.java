9eb8eb574373e36e85ea1390398013d843c39213
unknown <ahboe579@gmail.com>
2021-01-02 21:24:10 +0800

    added javadoc at Shop.java

--- Shop.java
+++ Shop.java
@@ -1,20 +1,46 @@
+/**
+ * A shop object representing the shop's name, phone, status and manager which
+ * extends from Role.java, where name, phone, status and manager are strings.
+ */
 public class Shop extends Role {
   private static final long serialVersionUID = 1L;
   private String manager;
 
+  /**
+   * Construct a shop with it's name, phone, status and manager are set to null
+   * ("")
+   */
   public Shop() {
 
   };
 
+  /**
+   * Construct a shop with specified name, phone, status and manager.
+   * 
+   * @param name    the name of the shop
+   * @param phone   the phone number of the shop
+   * @param status  the status of the shop
+   * @param manager the manager's name of the shop
+   */
   public Shop(String name, String phone, String status, String manager) {
     super(name, phone, status);
     this.manager = manager;
   }
 
+  /**
+   * Returns the manager's name from the specified shop.
+   * 
+   * @return the name of the manager
+   */
   public String getManager() {
     return manager;
   }
 
+  /**
+   * Returns the shop in name phone status manager.
+   * 
+   * @return name phone status manager
+   */
   @Override
   public String toString() {
     return super.toString() + " " + manager;
