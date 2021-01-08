80e20d5930e8cd924698fd2a788c8cb18ff466fa
Chan Yun Hong <yhchan0918@gmail.com>
2021-01-09 00:02:16 +0800

    change admintitle

--- AdminMain.java
+++ AdminMain.java
@@ -2,34 +2,34 @@ import java.util.List;
 import java.util.Arrays;
 
 public class AdminMain {
-    public static void main(String[] args) throws Exception {
-        menu();
-    }
+  public static void main(String[] args) throws Exception {
+    menu();
+  }
 
-    public static void menu() throws Exception {
-        Utils.displayHeader("Welcome back admin");
-        System.out.println("Please choose an action");
-        System.out.println("1. View Data");
-        System.out.println("2. Flag");
-        System.out.println("3. Generate 30 Visits");
-        System.out.println("4. Exit");
+  public static void menu() throws Exception {
+    Utils.displayHeader("Welcome Admin");
+    System.out.println("Please choose an action");
+    System.out.println("1. View Data");
+    System.out.println("2. Flag");
+    System.out.println("3. Generate 30 Visits");
+    System.out.println("4. Exit");
 
-        List<Integer> options = Arrays.asList(1, 2, 3, 4);
-        int choice = Utils.getUserChoice(options);
-        switch (choice) {
-            case 1:
-                AdminView.menu();
-                break;
-            case 2:
-                AdminFlag.menu();
-                break;
-            case 3:
-                AdminGenerate.menu();
-                break;
-            case 4:
-                System.out.println("Exiting...");
-                System.out.println("See you again, Admin");
-                break;
-        }
+    List<Integer> options = Arrays.asList(1, 2, 3, 4);
+    int choice = Utils.getUserChoice(options);
+    switch (choice) {
+      case 1:
+        AdminView.menu();
+        break;
+      case 2:
+        AdminFlag.menu();
+        break;
+      case 3:
+        AdminGenerate.menu();
+        break;
+      case 4:
+        System.out.println("Exiting...");
+        System.out.println("See you again, Admin");
+        break;
     }
+  }
 }
